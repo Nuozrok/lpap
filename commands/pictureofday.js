d2fd258aa6653ca868c2f90672d40f98758a48bb
whig123 <mwong940@gmail.com>
2023-01-24 21:49:54 -0500

    version 1 of soup feature of the day

--- commands/pictureofday.js
+++ commands/pictureofday.js
@@ -8,12 +8,21 @@ module.exports = {
 		.setDescription('Provides a picture of the day.'),
 
 	async execute(interaction) {
+
+		// TODO use a google images api to pull a bunch of soup images
+		
+
+		// create actual embedder
 		const embedder = new EmbedBuilder()
 			.setColor(0x0099FF)
-			.setTitle('Soup of Day')
-			.setAuthor({ name: 'BOT',  iconURL: 'https://i.imgur.com/AfFp7pu.png' })
-			.setDescription('Soup of Day')
-			.setImage('https://i.imgur.com/AfFp7pu.png')
+			.setTitle('Wants this Soup')
+			.setAuthor({ name: interaction.user.username,  iconURL: interaction.user.avatarURL() })
+			.setDescription('Soup of Day: ')
+			.setImage('https://d7hftxdivxxvm.cloudfront.net/?height=1600&quality=50&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FyOaSKJM3HCtpqUMCG38MVA%2Fnormalized.jpg&width=1068')
 			.setTimestamp()
-		await interaction.reply({ content: 'picture of day', embeds: [embedder] }); },
+
+
+
+		await interaction.reply({ content: 'picture of day', embeds: [embedder] });
+	},
 };
