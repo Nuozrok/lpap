d204211028b97cc632df928431ccd8ff7c40cb2d
Nuozrok <1korzoun1@gmail.com>
2023-04-14 14:46:22 -0400

    remove and replace django secret key
    oops

--- web/lpap/lpap/settings.py
+++ web/lpap/lpap/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p#jehz$g2vdup@xq5%2rh3%qc^ys#z37_qzdt8*)%^0vna=4*z'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
