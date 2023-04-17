714adbc4c3a38831f95c6c50c5c41ebb8e8efc81
Nuozrok <1korzoun1@gmail.com>
2023-04-17 15:52:41 -0400

    Update settings.py

--- web/lpap/lpap/settings.py
+++ web/lpap/lpap/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
