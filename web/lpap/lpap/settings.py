ed1b2e8b4a2c79fe7476e9afae98afe891cf3918
Nuozrok <1korzoun1@gmail.com>
2023-12-02 23:48:45 -0500

    no idea, i just want push changes and checkout

--- web/lpap/lpap/settings.py
+++ web/lpap/lpap/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
